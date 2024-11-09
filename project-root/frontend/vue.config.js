ee8bddcb85922968b270573a1edfcc44dc1440ee
Mohammed-Ammar Hassnou <st188386@stud.uni-stuttgart.de>
2024-11-09 22:46:15 +0100

    Update vue.config.js

--- project-root/frontend/vue.config.js
+++ project-root/frontend/vue.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
-    port: 8081
+    port: 8081,
+    host: '0.0.0.0',  // Allows access from all network interfaces
   }
-};
+})
