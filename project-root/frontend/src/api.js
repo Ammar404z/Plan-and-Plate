5085c88280fe7b848b18fb3a6f4cfef7e0991dcd
Mohammed-Ammar Hassnou <st188386@stud.uni-stuttgart.de>
2024-11-10 00:58:52 +0100

    hotfix

--- project-root/frontend/src/api.js
+++ project-root/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL,  // Uses the IP of the VM
+  baseURL: 'http://193.196.52.222:8080',  // Directly using the IP of the VM
 });
 
 export default api;
