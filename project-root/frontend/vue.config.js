c34a98f461a1764458abef27fbaad90119f5fb9b
Mohammed-Ammar Hassnou <st188386@stud.uni-stuttgart.de>
2024-11-10 18:43:22 +0100

    configure to ipv6

--- project-root/frontend/vue.config.js
+++ project-root/frontend/vue.config.js
@@ -4,14 +4,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 8083,
-    host: '0.0.0.0',
+    host: '::',  // Allow both IPv4 and IPv6 access
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
     hot: false,           // Disable Hot Module Replacement
     liveReload: false,    // Disable Live Reload
     client: {
-      webSocketURL: 'ws://193.196.52.222:8083/ws',
+      webSocketURL: 'ws://[2001:7c0:2320:1:f816:3eff:fe09:d4aa]:8083/ws',  // Use IPv6 with brackets
     },
   },
 });
